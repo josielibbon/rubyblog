eb92f83139d309297136968776bfec955ad7da12
Josie Libbon <josielibbon@gwu.edu>
2022-12-30 04:33:41 -0500

    can see all articles and edit them

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,22 @@ class ArticlesController < ApplicationController
     def show
     @article = Article.find(params[:id])
     end
+    def edit
+        @article = Article.find(params[:id])
+      end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = "Article was not updated"
+            render 'edit'
+        end
+    end
+    def index
+        @articles = Article.all
+      end
     private
       def article_params
        params.require(:article).permit(:title, :description)
